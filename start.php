42de98ae28ee998c4639e0e949d0b920399875a0
Arata Komiya <komiyaarata50@gmail.com>
2020-08-11 14:13:09 +0900

    all added

--- start.php
+++ start.php
@@ -21,7 +21,7 @@
 　　　<div class="pic">
        <a href="toushin_index.php"><img src="img/toushin.jpeg" title="東進英単語"></a>
        <a href="target_5_index.php"><img src="img/target_5.jpeg" title="ターゲット_5英単語"></a>
-       <a href="target_5_index.php"><img src="img/target_6.jpeg" title="ターゲット_6英単語"></a>
+       <a href="target_6_index.php"><img src="img/target_6.jpeg" title="ターゲット_6英単語"></a>
      </div>
 </body>
 </html>
