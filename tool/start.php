9ae23a52d13f17b03d55bc39f228394258c6f652
Arata Komiya <komiyaarata50@gmail.com>
2020-08-12 18:02:17 +0900

    all added

--- tool/start.php
+++ tool/start.php
@@ -20,9 +20,13 @@
     <h1>単語テスト</h1>
 　　　<div class="pic">
        <a href="../toushin/toushin_index.php"><img src="../img/toushin.jpeg" title="東進英単語"></a>
+       <a href="../toushin_i/toushin_i_index.php"><img src="../img/toushin_i.jpeg" title="東進英熟語"></a>
        <a href="../target_5/target_5_index.php"><img src="../img/target_5.jpeg" title="ターゲット_5英単語"></a>
        <a href="../target_6/target_6_index.php"><img src="../img/target_6.jpeg" title="ターゲット_6英単語"></a>
        <a href="../kanji/kanji_index.php"><img src="../img/kanji.png" title="漢字"></a>
+       <a href="../system/system_index.php"><img src="../img/system.jpg" title="システム英単語"></a>
+       <a href="../duo/duo_index.php"><img src="../img/duo.jpeg" title="DUO"></a>
+       <a href="../sokutan/sokutan_index.php"><img src="../img/sokudoku.jpg" title="速読英単語"></a>
      </div>
 </body>
 </html>
