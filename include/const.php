c0fa745cb29b391282580dbf10578fcde9729f28
Arata Komiya <komiyaarata50@gmail.com>
2020-08-17 19:58:43 +0900

    all added

--- include/const.php
+++ include/const.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST',   'mysql'); // データベースのホスト名又はIPアドレス
-define('DB_USER',   'root');  // MySQLのユーザ名
-define('DB_PASSWD', 'root');    // MySQLのパスワード
-define('DB_NAME',   'practice');    // データベース名
+define('DB_HOST',   'mysql1028.db.sakura.ne.jp'); // データベースのホスト名又はIPアドレス
+define('DB_USER',   'cram-school');  // MySQLのユーザ名
+define('DB_PASSWD', 'cram-school');    // MySQLのパスワード
+define('DB_NAME',   'cram-school_practice');    // データベース名
  
 define('HTML_CHARACTER_SET', 'UTF-8');  // HTML文字エンコーディング
 define('DB_CHARACTER_SET',   'UTF8');   // DB文字エンコーディング
--- tool/start.php
+++ tool/start.php
@@ -53,16 +53,18 @@ if (isset($_SESSION['id']) === TRUE) {
 　　　<div class="pic">
        <a href="../kanji/kanji_index.php"><img src="../img/kanji.png" title="小中学漢字"></a>
        <a href="../junior_en/junior_en_index.php"><img src="../img/junior_en.png" title="中学英単語"></a>
-       <a href="../toushin/toushin_index.php"><img src="../img/toushin.jpeg" title="東進英単語"></a>
-       <a href="../toushin_i/toushin_i_index.php"><img src="../img/toushin_i.jpeg" title="東進英熟語"></a>
        <a href="../target_5/target_5_index.php"><img src="../img/target_5.jpeg" title="ターゲット_5英単語"></a>
        <a href="../target_6/target_6_index.php"><img src="../img/target_6.jpeg" title="ターゲット_6英単語"></a>
        <a href="../system/system_index.php"><img src="../img/system.jpg" title="システム英単語"></a>
        <a href="../system_5/system_5_index.php"><img src="../img/system_5jpeg.jpeg" title="システム英単語＿5"></a>
        <a href="../sokutan/sokutan_index.php"><img src="../img/sokudoku.jpg" title="速読英単語"></a>
        <a href="../sokujuku/sokujuku_index.php"><img src="../img/sokujuku.jpeg" title="速読英熟語"></a>
+       <a href="../senior/senior_index.php"><img src="../img/senior.jpg" title="速読英単語上級"></a>
        <a href="../duo/duo_index.php"><img src="../img/duo.jpeg" title="DUO"></a>
+       <a href="../toushin/toushin_index.php"><img src="../img/toushin.jpeg" title="東進英単語"></a>
+       <a href="../toushin_i/toushin_i_index.php"><img src="../img/toushin_i.jpeg" title="東進英熟語"></a>
        <a href="../kobun315/kobun315_index.php"><img src="../img/kobun315.jpeg" title="古文単語315"></a>
+       <a href="../madonna/madonna_index.php"><img src="../img/madonna.jpg" title="マドンナ古文単語"></a>
      </div>
 </body>
 </html>
