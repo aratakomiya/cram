5fb38d6bd04bc5f6e192c690a1384d1c11181aea
Arata Komiya <komiyaarata50@gmail.com>
2020-08-19 13:57:44 +0900

    all added

--- tool/start.php
+++ tool/start.php
@@ -65,6 +65,7 @@ if (isset($_SESSION['id']) === TRUE) {
        <a href="../toushin_i/toushin_i_index.php"><img src="../img/toushin_i.jpeg" title="東進英熟語"></a>
        <a href="../kobun315/kobun315_index.php"><img src="../img/kobun315.jpeg" title="古文単語315"></a>
        <a href="../madonna/madonna_index.php"><img src="../img/madonna.jpg" title="マドンナ古文単語"></a>
+       <a href="../tunagaru/tunagaru_index.php"><img src="../img/tunagaru.jpeg" title="つながる、まとまる古文単語500"></a>
        
      </div>
 </body>
